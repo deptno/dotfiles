0f27fddd90f258b53c5d25c7c0be3d7d20409d42
deptno <deptno@gmail.com>
2026-02-19 17:58:53 +0900

    support neo-tree document_symbols

--- config/.config/nvim/lua/plugins/neo-tree.lua
+++ config/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,20 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
+      opts.sources = { "filesystem", "buffers", "git_status", "document_symbols" }
+      opts.document_symbols = {
+        follow_cursor = true,
+      }
+      opts.source_selector = vim.tbl_deep_extend("force", opts.source_selector or {}, {
+        winbar = true, -- 또는 statusline = true
+        sources = {
+          { source = "filesystem" },
+          { source = "buffers" },
+          { source = "git_status" },
+          { source = "document_symbols" },
+        },
+      })
+
       local ok_utils, utils = pcall(require, "neo-tree.utils")
       local ok_ls, ls_files = pcall(require, "neo-tree.git.ls-files")
       if ok_utils and ok_ls and not ls_files._home_gitignore_patched then
